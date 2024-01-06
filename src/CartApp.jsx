3c1800c812e003346929f1e3b7d70ff4e16f5855
framirez91 <framirez.d85@gmail.com>
2024-01-06 20:03:01 -0300

    Add react-router-dom dependency and refactor CartApp component

--- src/CartApp.jsx
+++ src/CartApp.jsx
@@ -1,45 +1,13 @@
-import { useReducer, useEffect } from "react"
+
 import { CartView } from "./components/CartView"
 import { CatalogView } from "./components/CatalogView"
-import { AddProductCart, DeleteProductCart, UpdateQuantityProductCart } from "./reducer/itemsActions";
-import { itemsReducer } from "./reducer/itemsReducer";
-
-const inititalCartItems = JSON.parse(sessionStorage.getItem('cart')) || [];
+import { useItemsCart } from "./hooks/useItemsCart";
 
 export const CartApp = () => {
 
-    const [cartItems, dispatch] = useReducer(itemsReducer, inititalCartItems);
+    const {cartItems,handlerAddProductCart,handlerDeleteProductCart} = useItemsCart();
 
-    useEffect(() => {
-        sessionStorage.setItem('cart', JSON.stringify(cartItems));
-    }, [cartItems]);
-
-    const handlerAddProductCart = (product) => {
-
-        const hasItem = cartItems.find((i) => i.product.id === product.id);
-        if (hasItem) {
-            dispatch(
-                {
-                    type: UpdateQuantityProductCart,
-                    payload: product,
-                }
-            );
-        } else {
-            dispatch({
-                type: AddProductCart,
-                payload: product,
-            });
-        }
-    }
-
-    const handlerDeleteProductCart = (id) => {
-        dispatch(
-            {
-                type: DeleteProductCart,
-                payload: id,
-            }
-        )
-    }
+    
     return (
         <>
 
